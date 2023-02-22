18ad2c64cf28ce0e0e314deb70270a4cd37b9cf3
davielachesis <91736880+davielachesis@users.noreply.github.com>
2023-02-22 13:49:32 -0300

    First version upload

---
+++ assets/js/exalted.js
@@ -0,0 +1,41 @@
+document.getElementById('roll').addEventListener('click', exalted)
+const resultInput = document.getElementById('result')
+const logsInput = document.getElementById('logBox')
+const diceInput = document.getElementById('diceInput')
+
+
+  const log = []
+function exaltedD10(){
+  const d10 = Math.floor(Math.random()*10 + 1)
+  let result = ''
+  if(d10 == 10){
+    const print = 2
+    resultInput.style.backgroundColor = '#ff0f'
+    result = 'Two Successes'
+    console.log(d10 + ' | ' + result)
+    log.push(print)
+  }else if(d10 >= 7){
+    resultInput.style.backgroundColor = '#1b0f'
+    const print = 1
+    result = 'Success'
+    console.log(d10 + ' | ' + result)
+    log.push(print)
+  }else{
+    resultInput.style.backgroundColor = '#ff0000'
+    const print = 0
+    result = 'Fail'
+    console.log(d10 + ' | ' + result)
+    log.push(print)
+  }
+  logsInput.value = log
+  resultInput.value = d10
+  return d10 
+  
+}
+
+function exalted(){
+for(let i = 1; i <= diceInput.value; i++){
+  exaltedD10()
+}
+
+}
